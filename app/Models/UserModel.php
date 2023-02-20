a1f02bc4e0d456ec915ad862758edc844dbf7be6
israel_manzi <manziisrael99@gmail.com>
2023-02-20 16:12:10 +0200

    [feat]: auth & middlware

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,9 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class UserModel extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +20,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'user_id',
+        'username',
         'email',
         'password',
     ];
@@ -41,4 +44,33 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getUser($userId)
+    {
+        return $this->where('user_id', $userId)->first();
+    }
+
+    public function getUserByUsername($username)
+    {
+        return $this->where('username', $username)->first();
+    }
+
+    public function createUser($user)
+    {
+        return $this->create($user);
+    }
+
+    public function updateUser($userId, $user)
+    {
+        $user = $this->where('user_id', $userId)->first()->udpate($user);
+
+        if ($user) {
+            return $this->getUser($userId);
+        }
+    }
+
+    public function deleteUser($userId)
+    {
+        return $this->where('user_id', $userId)->delete();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CheckAuthentication;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 //default route
 Route::get('/', function () {
@@ -25,18 +26,28 @@ Route::get('/', function () {
     ]);
 });
 
-//albums routes
-Route::get('/albums', [App\Http\Controllers\AlbumController::class, 'getAlbums']);
-Route::get('/albums/{id}', [App\Http\Controllers\AlbumController::class, 'getAlbum']);
-Route::post('/albums', [App\Http\Controllers\AlbumController::class, 'createAlbum']);
-Route::patch('/albums/{id}', [App\Http\Controllers\AlbumController::class, 'updateAlbum']);
-Route::delete('/albums/{id}', [App\Http\Controllers\AlbumController::class, 'deleteAlbum']);
+//user auth routes
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('/user', [App\Http\Controllers\UserController::class, 'getUserData']);
+    Route::post('/login', [App\Http\Controllers\UserController::class, 'login'])->name('login');
+    Route::post('/register', [App\Http\Controllers\UserController::class, 'registerUser']);
+    Route::post('/logout', [App\Http\Controllers\UserController::class, 'logout']);
+});
 
-//songs routes
-Route::get('/songs', [App\Http\Controllers\SongController::class, 'getSongs']);
-Route::get('/songs/{id}', [App\Http\Controllers\SongController::class, 'getSong']);
-Route::post('/songs', [App\Http\Controllers\SongController::class, 'createSong']);
-Route::patch('/songs/{id}', [App\Http\Controllers\SongController::class, 'updateSong']);
-Route::delete('/songs/{id}', [App\Http\Controllers\SongController::class, 'deleteSong']);
-
-Route::get('/albums/{id}/songs', [App\Http\Controllers\AlbumController::class, 'getAlbumSongs']);
+Route::group(['middleware' => CheckAuthentication::class], function () {
+    //albums routes
+    Route::get('/albums', [App\Http\Controllers\AlbumController::class, 'getAlbums']);
+    Route::get('/albums/{id}', [App\Http\Controllers\AlbumController::class, 'getAlbum']);
+    Route::post('/albums', [App\Http\Controllers\AlbumController::class, 'createAlbum']);
+    Route::patch('/albums/{id}', [App\Http\Controllers\AlbumController::class, 'updateAlbum']);
+    Route::delete('/albums/{id}', [App\Http\Controllers\AlbumController::class, 'deleteAlbum']);
+    
+    //songs routes
+    Route::get('/songs', [App\Http\Controllers\SongController::class, 'getSongs']);
+    Route::get('/songs/{id}', [App\Http\Controllers\SongController::class, 'getSong']);
+    Route::post('/songs', [App\Http\Controllers\SongController::class, 'createSong']);
+    Route::patch('/songs/{id}', [App\Http\Controllers\SongController::class, 'updateSong']);
+    Route::delete('/songs/{id}', [App\Http\Controllers\SongController::class, 'deleteSong']);
+    
+    Route::get('/albums/{id}/songs', [App\Http\Controllers\AlbumController::class, 'getAlbumSongs']);
+});
